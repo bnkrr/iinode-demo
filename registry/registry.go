1d984ad62954dfd0e2026cf139b8e934105e2804
dlchang <chdlgs@gmail.com>
2024-03-14 09:00:20 +0000

    add some comments

--- registry/registry.go
+++ registry/registry.go
@@ -1,5 +1,9 @@
 package main
 
+// @Title        registry.go
+// @Description  一个简单的供调试的注册服务器demo，只支持注册一个服务，并会周期性调用服务，打印输出
+// @Create       dlchang (2024/03/14 16:30)
+
 import (
 	"context"
 	"flag"
@@ -22,12 +26,19 @@ var (
 	registryAddress = flag.String("addr", "localhost:48001", "Listen address in the format of host:port")
 )
 
+// RegistryServer 供调试的注册服务器demo
 type RegistryServer struct {
-	pb.UnimplementedRegistryServer
-	servicePort  int
-	returnStream bool
+	pb.UnimplementedRegistryServer      // 来自rpc生成代码
+	servicePort                    int  // 用于存储service的端口
+	returnStream                   bool // 用户存储service是否是流服务
 }
 
+// @title        RegisterService
+// @description  rpc接口实现，注册一个服务
+// @auth         dlchang (2024/03/14 16:30)
+// @param        ctx   context.Context   上下文对象
+// @param        req   *pb.RegisterServiceRequest   服务注册请求的rpc生成类型，内含服务各项信息，比如端口、是否是流调用等等。
+// @return       *pb.RegisterServiceResponse   服务注册响应的rpc生成类型
 func (s *RegistryServer) RegisterService(ctx context.Context, req *pb.RegisterServiceRequest) (*pb.RegisterServiceResponse, error) {
 	log.Printf("%s listen at localhost:%d\n", req.Name, req.Port)
 	s.servicePort = int(req.Port)
@@ -35,6 +46,9 @@ func (s *RegistryServer) RegisterService(ctx context.Context, req *pb.RegisterSe
 	return &pb.RegisterServiceResponse{Message: "success"}, nil
 }
 
+// @title        Run
+// @description  调用注册service提供的普通函数调用接口
+// @auth         dlchang (2024/03/14 16:30)
 func (s *RegistryServer) Run() {
 	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", s.servicePort), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -50,6 +64,9 @@ func (s *RegistryServer) Run() {
 	}
 }
 
+// @title        RunStream
+// @description  调用注册service提供的返回流的调用接口
+// @auth         dlchang (2024/03/14 16:30)
 func (s *RegistryServer) RunStream() {
 	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", s.servicePort), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -74,6 +91,9 @@ func (s *RegistryServer) RunStream() {
 	}
 }
 
+// @title        RunRoutine
+// @description  根据service提供的调用类型，定期调用注册service提供的调用接口
+// @auth         dlchang (2024/03/14 16:30)
 func (s *RegistryServer) RunRoutine() {
 	for {
 		if s.servicePort < 0 {
@@ -88,6 +108,9 @@ func (s *RegistryServer) RunRoutine() {
 	}
 }
 
+// @title        main
+// @description  运行RegistryServer的grpc服务器
+// @auth         dlchang (2024/03/14 16:30)
 func main() {
 	flag.Parse()
 	listener, err := net.Listen(Network, *registryAddress)
--- service/service.go
+++ service/service.go
@@ -1,5 +1,9 @@
 package main
 
+// @Title        service.go
+// @Description  一个服务抽象的样例，支持自动向registry注册，并接入不同的服务实现
+// @Create       dlchang (2024/03/14 16:30)
+
 import (
 	"context"
 	"flag"
@@ -22,31 +26,49 @@ var (
 	registryAddress = flag.String("reg-addr", "localhost:48001", "Registry address in the format of host:port")
 )
 
+// ServiceHandler 具体服务功能实现的接口定义
 type ServiceHandler interface {
-	Name() string
-	Call(context.Context, *pb.ServiceCallRequest) (*pb.ServiceCallResponse, error)
-	CallStream(*pb.ServiceCallRequest, pb.Service_CallStreamServer) error
-	Version() string
-	Concurrency() int32
-	ReturnStream() bool
+	Name() string                                                                  // 服务名称
+	Call(context.Context, *pb.ServiceCallRequest) (*pb.ServiceCallResponse, error) // 服务的普通调用实现
+	CallStream(*pb.ServiceCallRequest, pb.Service_CallStreamServer) error          // 服务的流调用实现
+	Version() string                                                               // 服务版本
+	Concurrency() int32                                                            // 服务支持的并发
+	ReturnStream() bool                                                            // 服务是否实现了流调用
 }
 
+// BaseService 具体服务功能实现的接口定义
 type BaseService struct {
-	pb.UnimplementedServiceServer
-	service         ServiceHandler
-	registryAddress *string
-	netListener     *net.Listener
-	registryClient  pb.RegistryClient
+	pb.UnimplementedServiceServer                   // 来自grpc自动生成代码
+	service                       ServiceHandler    // 实现的服务接口
+	registryAddress               *string           // registry的侦听地址
+	netListener                   *net.Listener     // 用于存储service的侦听
+	registryClient                pb.RegistryClient // 用于调用registry的grpc client
 }
 
+// @title        Call
+// @description  rpc接口实现，服务普通调用，用于实现一次性输入输出的服务，如测量一个IP地址是否有指定漏洞
+// @auth         dlchang (2024/03/14 16:30)
+// @param        ctx   context.Context   上下文对象
+// @param        req   *pb.ServiceCallRequest   调用请求的rpc生成类型，内含输入Input字符串。
+// @return       *pb.ServiceCallResponse  调用响应的rpc生成类型，内含输出Output字符串
+// @return       error   错误
 func (s *BaseService) Call(ctx context.Context, req *pb.ServiceCallRequest) (*pb.ServiceCallResponse, error) {
 	return s.service.Call(ctx, req)
 }
 
+// @title        CallStream
+// @description  rpc接口实现，服务流调用，用于实现返回一个流的服务，如测量一段IP地址是否有活跃端口
+// @auth         dlchang (2024/03/14 16:30)
+// @param        req   *pb.ServiceCallRequest   调用请求的rpc生成类型，内含输入Input字符串。
+// @param        stream pb.Service_CallStreamServer  调用响应的rpc生成类型，是一个流
+// @return       error   错误
 func (s *BaseService) CallStream(req *pb.ServiceCallRequest, stream pb.Service_CallStreamServer) error {
 	return s.service.CallStream(req, stream)
 }
 
+// @title        Serve
+// @description  启动一个服务的主函数
+// @auth         dlchang (2024/03/14 16:30)
 func (s *BaseService) Serve() {
 	listener, err := net.Listen(Network, ServiceAddress)
 	if err != nil {
@@ -61,6 +83,9 @@ func (s *BaseService) Serve() {
 	grpcServer.Serve(*s.netListener)
 }
 
+// @title        Register
+// @description  向registry注册自己的端口等服务信息
+// @auth         dlchang (2024/03/14 16:30)
 func (s *BaseService) Register() {
 	conn, err := grpc.Dial(*s.registryAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -85,6 +110,9 @@ func (s *BaseService) Register() {
 	}
 }
 
+// @title        Register
+// @description  周期性向registry注册
+// @auth         dlchang (2024/03/14 16:30)
 func (s *BaseService) RegisterRoutine() {
 	for {
 		s.Register()
@@ -92,12 +120,18 @@ func (s *BaseService) RegisterRoutine() {
 	}
 }
 
+// @title        NewService
+// @description  新建一个服务
+// @auth         dlchang (2024/03/14 16:30)
 func NewService(registryAddress *string, service ServiceHandler) (*BaseService, error) {
 	s := &BaseService{registryAddress: registryAddress, service: service}
 
 	return s, nil
 }
 
+// @title        main
+// @description  主函数，处理flag
+// @auth         dlchang (2024/03/14 16:30)
 func main() {
 	flag.Parse()
 	s, err := NewService(registryAddress, &EchoService{}) // 改动此处服务类型
